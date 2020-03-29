041ae699f1779e92496884b0dfe832c798c8a199
Kent C. Dodds <me+github@kentcdodds.com>
2020-03-28 22:39:39 -0600

    exercises/04-authentication

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -1,45 +1,6 @@
-import {queryCache} from 'react-query'
-const localStorageKey = '__bookshelf_token__'
+export * from './api-client.final'
 
-function client(endpoint, {body, ...customConfig} = {}) {
-  const token = window.localStorage.getItem(localStorageKey)
-  const headers = {'content-type': 'application/json'}
-  if (token) {
-    headers.Authorization = `Bearer ${token}`
-  }
-  const config = {
-    method: body ? 'POST' : 'GET',
-    ...customConfig,
-    headers: {
-      ...headers,
-      ...customConfig.headers,
-    },
-  }
-  if (body) {
-    config.body = JSON.stringify(body)
-  }
+// export * from './api-client.exercise'
 
-  return window
-    .fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
-    .then(async r => {
-      if (r.status === 401) {
-        logout()
-        // refresh the page for them
-        window.location.assign(window.location)
-        return
-      }
-      const data = await r.json()
-      if (r.ok) {
-        return data
-      } else {
-        return Promise.reject(data)
-      }
-    })
-}
-
-function logout() {
-  queryCache.clear()
-  window.localStorage.removeItem(localStorageKey)
-}
-
-export {client, localStorageKey, logout}
+// 💯 automatically logout on 401
+// export * from './api-client.extra-3'
