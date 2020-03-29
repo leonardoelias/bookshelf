695f21cf9c22524daddff7abde49cb34f926823b
Kent C. Dodds <me+github@kentcdodds.com>
2020-03-28 22:39:39 -0600

    exercises/05-routing

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,18 +1,47 @@
-import {within, waitForElementToBeRemoved} from '@testing-library/react'
-import ReactDOM from 'react-dom'
+import {screen, waitForElementToBeRemoved, userEvent} from 'test/app-test-utils'
+import {buildUser} from 'test/generate'
 
-test('booting up the app from the index file does not break anything', async () => {
-  // setup
-  const div = document.createElement('div')
-  div.setAttribute('id', 'root')
-  document.body.appendChild(div)
+beforeAll(() => {
+  const root = document.createElement('div')
+  root.id = 'root'
+  document.body.append(root)
+  document.body.focus()
+})
 
-  // run the file and wait for things to settle.
-  require('..')
-  const {getByLabelText} = within(document.body)
-  await waitForElementToBeRemoved(() => getByLabelText(/loading/i))
+test('can login and use the book search', async () => {
+  require('../index')
 
-  // cleanup
-  ReactDOM.unmountComponentAtNode(div)
-  document.body.removeChild(div)
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  const user = buildUser()
+
+  userEvent.click(screen.getByRole('button', {name: /register/i}))
+  await userEvent.type(screen.getByLabelText(/username/i), user.username)
+  await userEvent.type(screen.getByLabelText(/password/i), user.password)
+
+  userEvent.click(screen.getByRole('button', {name: /register/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  userEvent.click(screen.getByText(/discover/i))
+
+  const searchInput = screen.getByPlaceholderText(/search/i)
+  await userEvent.type(searchInput, 'voice of war')
+
+  userEvent.click(screen.getByLabelText(/search/i))
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  userEvent.click(screen.getByText(/voice of war/i))
+
+  expect(window.location.href).toMatchInlineSnapshot(
+    `"http://localhost/book/B084F96GFZ"`,
+  )
+
+  expect(
+    await screen.findByText(/to the west, a sheltered girl/i),
+  ).toBeInTheDocument()
+
+  userEvent.click(screen.getByRole('button', {name: /logout/i}))
+
+  expect(searchInput).not.toBeInTheDocument()
 })
